6e506d0d3d8e0f39d470e497d44fb2ed425b1416
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-17 07:39:08 -0300

    Creating new class Aluno

--- alunos/models.py
+++ alunos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+SERIES_CHOICES = (
+    (8, 'Oitavo ano'),
+    (9, 'Nono Ano'),
+)
+
+
+class Aluno(AbstractUser):
+    date_joined = None
+    first_name = None
+    last_name = None
