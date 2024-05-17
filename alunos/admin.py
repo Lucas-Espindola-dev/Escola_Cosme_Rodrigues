c97d2cf5b88143db31fc2ae4c1904ad1e70db19f
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-17 10:14:59 -0300

    Adding fields to admin

--- alunos/admin.py
+++ alunos/admin.py
@@ -6,3 +6,6 @@ from .models import Aluno
 class CustomAlunoAdmin(UserAdmin):
     list_display = ['id', 'full_name', 'username', 'matricula', 'email', 'is_active', 'is_staff']
     list_display_links = ['id', 'full_name', 'username', 'matricula', ]
+    list_filter = ['is_active', 'is_staff', 'is_superuser', ]
+    search_fields = ['full_name', 'username', 'matricula', ]
+    ordering = ['full_name', 'username', ]
