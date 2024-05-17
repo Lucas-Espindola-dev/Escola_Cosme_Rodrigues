b90b6eafb7d1d2740cadac241d4874481613ddf8
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-17 10:17:09 -0300

    Adding fieldsets to admin

--- alunos/admin.py
+++ alunos/admin.py
@@ -9,3 +9,34 @@ class CustomAlunoAdmin(UserAdmin):
     list_filter = ['is_active', 'is_staff', 'is_superuser', ]
     search_fields = ['full_name', 'username', 'matricula', ]
     ordering = ['full_name', 'username', ]
+    actions = ['activate_users', 'deactivate_users']
+    fieldsets = (
+        ("Informações de Acesso", {
+            "fields": (
+                "username",
+                "password"
+            )
+        }),
+        ("Informações Pessoais", {
+            "fields": (
+                "full_name",
+                "matricula",
+                "email",
+                "seduc",
+                "cpf",
+                "serie",
+                "telefone",
+                "mother_name",
+                "father_name",
+            )
+        }),
+        ("Permissões", {
+            "fields": (
+                "groups",
+                "user_permissions",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            )
+        }),
+    )
