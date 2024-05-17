b76a40575dca8f59ea3f8c08f1a34dcd796446df
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-17 10:02:29 -0300

    Creating the UserAdmin class

--- alunos/admin.py
+++ alunos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Aluno
 
-# Register your models here.
+
+class CustomAlunoAdmin(UserAdmin):
+    list_display = ['id', 'full_name', 'username', 'matricula', 'email', 'is_active', 'is_staff']
+    list_display_links = ['id', 'full_name', 'username', 'matricula', ]
