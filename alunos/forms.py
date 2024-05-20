0fdea19b21d4b4901d1113188ff3f54cb3def5db
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-20 09:54:24 -0300

    Creating a function to save the created user

--- alunos/forms.py
+++ alunos/forms.py
@@ -18,3 +18,10 @@ class AlunoCreationForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError('Senhas não identicas!')
         return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
