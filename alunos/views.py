ea63cca2a7267a54cc7b7d5e8eb306198ebd046e
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-20 09:37:17 -0300

    .

--- alunos/views.py
+++ alunos/views.py
@@ -6,12 +6,3 @@ def home(request):
     return render(request, 'pages/home.html')
 
 
-def register_view(request):
-    if request.method == 'POST':
-        user_form = UserCreationForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-            return redirect("login")
-    else:
-        user_form = UserCreationForm()
-    return render(request, 'register.html', {"user_form": user_form})
