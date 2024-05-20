32e58ef383c410fbaafc7ef3f134b037abf0aa9f
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-20 09:37:52 -0300

    Creating class meta with model and fields to the form

--- alunos/forms.py
+++ alunos/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from .models import Aluno
 
 
 class AlunoCreationForm(forms.ModelForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Aluno
+    fields = ('username', 'email', 'full_name',
+              'matricula', 'seduc', 'cpf', 'serie',
+              'telefone', 'mother_name', 'father_name',)
