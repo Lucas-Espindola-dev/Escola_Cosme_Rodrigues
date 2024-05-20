7567ed1dc9109daa42f4cc95546561e0122cc1f2
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-20 09:44:37 -0300

    Creating a function to verify if password 1 and 2 match.

--- alunos/forms.py
+++ alunos/forms.py
@@ -11,3 +11,10 @@ class AlunoCreationForm(forms.ModelForm):
     fields = ('username', 'email', 'full_name',
               'matricula', 'seduc', 'cpf', 'serie',
               'telefone', 'mother_name', 'father_name',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Senhas não identicas!')
+        return password2
