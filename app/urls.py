149ce0106cb63dea927705f41c83b91d2015f3d1
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-20 10:34:58 -0300

    Changing the view of register

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from alunos.views import home
+from alunos.views import home, AlunoRegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', home, name='home'),
+    path('register/', AlunoRegisterView.as_view(), name='register'),
 ]
