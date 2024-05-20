958cd35f057e9c6f99e6630411ecfad1eb7f6b7f
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-20 10:35:23 -0300

    Creating the view to register new students

--- alunos/views.py
+++ alunos/views.py
@@ -1,8 +1,19 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.views import View
+from .forms import AlunoCreationForm
 
 
 def home(request):
     return render(request, 'pages/home.html')
+
+
+class AlunoRegisterView(View):
+    form_class = AlunoCreationForm
+    initial = {'key': 'value'}
+    template_name = 'pages/register.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
 
 
