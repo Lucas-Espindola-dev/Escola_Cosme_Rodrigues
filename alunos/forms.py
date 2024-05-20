58d643aecbf73200ced7cfc3f3a2933bbb049dde
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-05-20 09:31:39 -0300

    Creating a form to login

---
+++ alunos/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class AlunoCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
