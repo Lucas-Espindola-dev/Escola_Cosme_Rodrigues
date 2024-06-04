3fd188183440f2adda9b36414994f5acc95ac9e1
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-06-04 12:24:22 -0300

    Adding the login and Logout Roots

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from alunos.views import home, AlunoRegisterView
+from alunos.views import home, AlunoRegisterView, AlunoLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', home, name='home'),
     path('register/', AlunoRegisterView.as_view(), name='register'),
+    path('login/', AlunoLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
