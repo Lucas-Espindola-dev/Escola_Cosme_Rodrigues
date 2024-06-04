40948ea4140d4f54d4cd5f70b9328aa1ee2b907c
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-06-04 12:36:01 -0300

    Excluding ALunoLoginForm

--- alunos/forms.py
+++ alunos/forms.py
@@ -28,15 +28,3 @@ class AlunoCreationForm(forms.ModelForm):
         return user
 
 
-class AlunoLoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    def clean(self):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
-        if username and password:
-            user = authenticate(username=username, password=password)
-            if not user:
-                raise forms.ValidationError('Login Inválido')
-        return super().clean()
