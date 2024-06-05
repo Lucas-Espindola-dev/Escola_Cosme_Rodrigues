a20a5384e16639eefdf413bbc8971856d1faa82e
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-06-05 08:46:56 -0300

    Creating post function to register

--- alunos/views.py
+++ alunos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import AlunoCreationForm
+from django.contrib.auth import authenticate
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -20,6 +21,13 @@ class AlunoRegisterView(View):
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(nome=username, password=raw_password)
+            return redirect('login')
+        return render(request, self.template_name, {'form': form})
 
 
 class AlunoLoginView(LoginView):
