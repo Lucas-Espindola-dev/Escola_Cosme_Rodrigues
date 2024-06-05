5e6c3233289afa9dc36ad077072321a9fdd38429
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-06-05 08:58:40 -0300

    Creating aluno detail root

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from alunos.views import home, AlunoRegisterView, AlunoLoginView
+from alunos.views import home, AlunoRegisterView, AlunoLoginView, AlunoDetailView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('register/', AlunoRegisterView.as_view(), name='register'),
     path('login/', AlunoLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('aluno/<int:pk>/', AlunoDetailView.as_view(), name='aluno_detail'),
 ]
